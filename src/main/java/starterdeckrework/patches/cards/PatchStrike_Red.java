ebd82b483465cd2d29bb068756b6bacc0985e4da
stormytuna <stormytuna@outlook.com>
2023-03-13 01:02:27 +0000

    Begun a rework
    * Will convert current cards into patches
    * This commit only does Strike_R and localization stuffs

---
+++ src/main/java/starterdeckrework/patches/cards/PatchStrike_Red.java
@@ -0,0 +1,60 @@
+package starterdeckrework.patches.cards;
+
+import basemod.ReflectionHacks;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.red.Strike_Red;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EnergizedBluePower;
+import starterdeckrework.StarterDeckRework;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class PatchStrike_Red {
+    private static final int ENERGY_NEXT_TURN = 1;
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("Strike_R");
+
+    @SpirePatch(clz = Strike_Red.class, method = "upgrade")
+    public static class Strike_Red_PrefixUpgrade {
+        @SpirePrefixPatch
+        public static SpireReturn patch(Strike_Red __instance) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+            if (StarterDeckRework.swapIroncladStrikes) {
+                Class<?> strikeRedClass = __instance.getClass();
+                Class<?> abstractCardClass = strikeRedClass.getSuperclass();
+
+                Method upgradeName = abstractCardClass.getDeclaredMethod("upgradeName");
+                upgradeName.setAccessible(true);
+                upgradeName.invoke(__instance);
+
+                Method upgradeDamage = abstractCardClass.getDeclaredMethod("upgradeDamage", int.class);
+                upgradeDamage.setAccessible(true);
+                upgradeDamage.invoke(__instance, 1);
+
+                __instance.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+                __instance.initializeDescription();
+
+                SpireReturn.Return(null);
+            }
+
+            return SpireReturn.Continue();
+        }
+    }
+
+    @SpirePatch(clz = Strike_Red.class, method = "use")
+    public static class Strike_Red_PostUse {
+        @SpirePostfixPatch
+        public static void patch(Strike_Red __instance, AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+            if (__instance.upgraded) {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(abstractPlayer, abstractPlayer, new EnergizedBluePower(abstractPlayer, ENERGY_NEXT_TURN)));
+            }
+        }
+    }
+}
---
+++ src/main/java/starterdeckrework/patches/localization/PatchCardStrings.java
@@ -0,0 +1,39 @@
+package starterdeckrework.patches.localization;
+
+import basemod.ReflectionHacks;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.localization.LocalizedStrings;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import javafx.scene.effect.Reflection;
+import starterdeckrework.StarterDeckRework;
+
+import java.util.Map;
+
+public class PatchCardStrings {
+    private static Map<String, CardStrings> cards;
+
+    @SpirePatch(clz = LocalizedStrings.class, method = "<ctor>")
+    public static class LocalizedStrings_PostfixCtor {
+        @SpirePostfixPatch
+        public static void patch(LocalizedStrings __instance) {
+            PatchCardStrings.cards = ReflectionHacks.getPrivate(__instance, LocalizedStrings.class, "cards");
+        }
+    }
+
+    @SpirePatch(clz = LocalizedStrings.class, method = "getCardStrings", paramtypez = {String.class})
+    public static class LocalizedStrings_PrefixGetCardStrings {
+        @SpirePrefixPatch
+        public static SpireReturn<CardStrings> patch(LocalizedStrings __instance, String cardName) {
+            CardStrings cardStrings = PatchCardStrings.cards.get(StarterDeckRework.makeID(cardName));
+            if (cardStrings != null) {
+                return SpireReturn.Return(cardStrings);
+            }
+
+            return SpireReturn.Continue();
+        }
+    }
+}
