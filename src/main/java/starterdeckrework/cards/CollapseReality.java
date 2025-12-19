473244750aef8ed076b3a5c63ead58af5dcb9795
stormytuna <stormytuna@outlook.com>
2025-12-19 02:36:23 +0000

    add collapse reality

---
+++ src/main/java/starterdeckrework/cards/CollapseReality.java
@@ -0,0 +1,54 @@
+package starterdeckrework.cards;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.cards.tempCards.Safety;
+import com.megacrit.cardcrawl.cards.tempCards.Smite;
+
+import starterdeckrework.util.CardInfo;
+
+import static starterdeckrework.StarterDeckRework.makeID;
+
+public class CollapseReality extends BaseCard {
+    private final static CardInfo cardInfo = new CardInfo(
+        "CollapseReality",
+        1,
+        CardType.SKILL,
+        CardTarget.SELF,
+        CardRarity.BASIC,
+        CardColor.PURPLE
+    );
+
+    public static final String ID = makeID(cardInfo.baseId);
+
+    public CollapseReality() {
+        super(cardInfo);
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+        AbstractCard card;
+        if (AbstractDungeon.player.stance.ID.equals("Wrath")) {
+            card = new Smite();
+        } else if (AbstractDungeon.player.stance.ID.equals("Calm")) {
+            card = new Safety();
+        } else {
+            card = new Miracle();
+        }
+
+        if (upgraded) {
+            card.upgrade();
+        }
+
+        addToBot(new MakeTempCardInHandAction(card));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new CollapseReality();
+    }
+}
